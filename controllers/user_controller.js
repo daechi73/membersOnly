c9489df506ab0a083735d10420891e80c01a80e0
J <59581793+daechi73@users.noreply.github.com>
2023-11-06 23:12:24 -0500

    implemented: sign-in-get, sign-in-post, sign-out, sign-up-get, sign-up-post

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const passport = require("passport");
+const HashedPassword = require("../public/javascripts/HashPassword");
 
 exports.profile = asyncHandler(async (req, res, next) => {
   res.send("profile implemented YEt");
@@ -13,22 +14,48 @@ exports.sign_in_get = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.sign_in_post = asyncHandler(async (req, res, next) => {
-  passport.authenticate("local", (err, user, options) => {
-    if (!user)
-      res.render("sign-in-get", {
-        title: "Sign-in",
-        errors: options.message,
-      });
-  })(req, res, next);
-});
+// exports.sign_in_post = asyncHandler(async (req, res, next) => {
+//   // passport.authenticate("local", (err, user, options) => {
+//   //   if (!user)
+//   //     res.render("sign-in-get", {
+//   //       title: "Sign-in",
+//   //       errors: options.message,
+//   //     });
+//   //   return {
+//   //     successRedirect: "/",
+//   //     failureRedirect: "/user/sign-in",
+//   //   };
+//   // })(req, res, next);
 
-// exports.sign_in_post = [
 //   passport.authenticate("local", {
 //     successRedirect: "/",
-//     failureRedirect: "/user/sign-in",
-//   }),
-// ];
+//     failureRedirect: "/",
+//     // failureFlash: true,
+//   });
+//   // passport.authenticate("local", {
+//   //   failureFlash: "Invalid username or password.",
+//   // });
+// });
+
+exports.sign_in_post = [
+  asyncHandler(async (req, res, next) => {
+    passport.authenticate("local", (err, user, options) => {
+      if (!user) {
+        res.render("sign-in-get", {
+          title: "Sign-in",
+          errors: options.message,
+        });
+      } else next();
+    })(req, res, next);
+  }),
+  (req, res, next) => {
+    console.log("onto the next");
+    next();
+  },
+  passport.authenticate("local", {
+    successRedirect: "/",
+  }),
+];
 
 // router.post("/login", (req, res, next) => {
 //   passport.authenticate("local",
@@ -53,15 +80,65 @@ exports.sign_up_get = asyncHandler(async (req, res, next) => {
     title: "Sign-up",
   });
 });
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-  res.send("sign_up not implemented yet");
-});
-
-// exports.sign_in_post = [
-//   body("password").trim().exists().withMessage("You must enter a password"),
-//   asyncHandler(async (req, res, next) => {
-//     const errors = validationResult(req);
-
-//     res.send("sign_in not implemented yet");
-//   }),
-// ];
+exports.sign_up_post = [
+  body("first_name")
+    .trim()
+    .exists()
+    .withMessage("You must Enter a first name")
+    .isLength({ min: 3 })
+    .withMessage("first name has to be atleast 3 characters long"),
+  body("last_name")
+    .trim()
+    .exists()
+    .withMessage("You must Enter a last name")
+    .isLength({ min: 3 })
+    .withMessage("last name has to be atleast 3 characters long"),
+  body("username")
+    .trim()
+    .exists()
+    .withMessage("You must Enter a username")
+    .isLength({ min: 3 })
+    .withMessage("Username has to be atleast 3 characters long")
+    .custom(async (value) => {
+      const user = await User.findOne({ user_name: value }).exec();
+      if (user) {
+        throw new Error("Username already in use");
+      }
+    }),
+  body("password")
+    .trim()
+    .exists()
+    .withMessage("You must enter a password")
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage(
+      "Password must be atleast 8 charaters that includes: 1 lowercase, 1 uppercase, 1 number, and 1 symbol"
+    ),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      user_name: req.body.username,
+      password: await HashedPassword(req.body.password),
+    });
+    if (!errors.isEmpty()) {
+      res.render("sign-up-get", {
+        title: "Sign-up",
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      console.log("here");
+      await user.save();
+      res.redirect("/");
+    }
+  }),
+];
