ac4fdd0dc8ad7390fe428e5c2b262ac7881df244
J <59581793+daechi73@users.noreply.github.com>
2023-11-06 23:07:48 -0500

    routed user/sign-in, user/signed-in, user/sign-up, user/sign-out, and user/:id

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ const user_controller = require("../controllers/user_controller");
 router.get("/sign-in", user_controller.sign_in_get);
 router.post("/signed-in", user_controller.sign_in_post);
 router.get("/sign-up", user_controller.sign_up_get);
+router.post("/sign-up", user_controller.sign_up_post);
 router.get("/sign-out", user_controller.sign_out);
 router.get("/:id", user_controller.profile);
 
