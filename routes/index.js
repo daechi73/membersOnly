1928a63bd08783394c5a5fe9ae2bcb92f337ec33
J <59581793+daechi73@users.noreply.github.com>
2023-11-07 23:28:22 -0500

    added a path for index.post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const comment_controller = require("../controllers/comment_controller");
-
+const upgrade_user_controller = require("../controllers/upgrade_user_controllers");
 /* GET home page. */
 router.get("/", comment_controller.comment_list);
-
+router.post("/", upgrade_user_controller.handle_post);
 module.exports = router;
